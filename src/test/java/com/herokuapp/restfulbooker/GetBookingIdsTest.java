9610ccbd04e7d818d82f5f1e84d820565370f70f
mgarc01 <mgarc01@adpr.pohzn.com>
2020-11-30 17:28:39 +0100

    nuevos tests con path parameters y query parameters

--- src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTest.java
+++ src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTest.java
@@ -29,4 +29,33 @@ public class GetBookingIdsTest extends BaseTest {
         List<Integer> bookingIds = response.jsonPath().getList("bookingId");
         Assert.assertFalse(bookingIds.isEmpty(), "List of booking ids should NOT be empty");
     }
+
+    @Test
+    public void getBookingIdsWithFilter(){
+        //Create booking
+        Response responseCreate = createBooking("Puli", "La maravillosa", 155, false,
+                "2020-11-30", "2020-12-03", "champagne" );
+        responseCreate.print();
+
+
+        //add query parameters
+        request.queryParam("firstname","Puli" );
+        request.queryParam("lastname", "La maravillosa");
+
+        //Get response with booking ids filtering by name
+        Response response = RestAssured
+                .given(request)
+                .get("/booking");
+
+        response.print();
+
+        //Verify that the response is 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status SHOULD be 200.");
+
+        //Verify that there is at least 1 booking id in the response
+        List<Integer> bookingIds = response.jsonPath().getList("bookingId");
+        Assert.assertFalse(bookingIds.isEmpty(), "List of booking ids should NOT be empty");
+    }
+
 }
+
